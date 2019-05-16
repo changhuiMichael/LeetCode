d995210fd3cd9a534737be0b80216bdb8e8e2307
ChanhuiLi <changhuiwork@outlook.com>
2019-05-16 22:52:01 +0800

    第二题

--- src/Array_bace/A27_Remove_Element.java
+++ src/Array_bace/A27_Remove_Element.java
@@ -1,7 +1,21 @@
 package Array_bace;
 
 public class A27_Remove_Element {
-	
+	class Solution {
+	    public int removeElement(int[] nums, int val) {
+	        if(nums.length == 0) return 0;
+	        int reval = 0;
+	        for(int i = 0; i < nums.length; i++) {
+	        	if(nums[i] != val) {
+	        	
+	        		nums[reval] = nums[i];
+	        		reval++;
+	        	}
+	        }
+	        return reval;
+
+	    }
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/Array_bace/A80_Remove_Duplicates_from_Sorted_Array_2.java
+++ src/Array_bace/A80_Remove_Duplicates_from_Sorted_Array_2.java
@@ -1,6 +1,24 @@
 package Array_bace;
 
 public class A80_Remove_Duplicates_from_Sorted_Array_2 {
+	public int removeDuplicates(int[] nums) {
+        if(nums.length < 2) return nums.length;
+        int res = 2;
+        //遍历一遍
+        
+        for(int i = 2; i < nums.length;i++) {
+        	if(nums[i] == nums[i-1]) {
+        		if(nums[i] != nums[res-2]){//这个地方做错了，做这种两个指针的问题特别要注意每个指针指向的是什么东西。进行比较的话也要想清楚和那个指针所代表的东西比较。
+        			nums[res++] = nums[i];
+        		}
+        		
+        	}else {
+        		nums[res] = nums[i];
+        		res++;
+        	}
+        }
+        return res;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
